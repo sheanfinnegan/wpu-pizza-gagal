1b236e458c88e0be9a7c0a9e6baeb9b00f181295
sheanfinnegan <sheanfinnegan2905@gmail.com>
2021-10-04 18:56:09 +0700

    break point

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $.getJSON("pizza.json", function (data) {
     let dataPizza = data.menu
     $.each(dataPizza, function (i, data) {
-        $('#daftar-menu').append('<div class="col-lg-4"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
+        $('#daftar-menu').append('<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
     })
 });
 
@@ -17,7 +17,7 @@ $('.nav-link').on('click', function (e) {
         $.getJSON("pizza.json", function (data) {
             let dataPizza = data.menu
             $.each(dataPizza, function (i, data) {
-                $('#daftar-menu').append('<div class="col-lg-4"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
+                $('#daftar-menu').append('<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
             })
         });
         return;
@@ -30,7 +30,7 @@ $('.nav-link').on('click', function (e) {
 
         $.each(dataPizza, function (i, data) {
             if (data.kategori == isiNav.toLowerCase()) {
-                content += '<div class="col-md-4"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>'
+                content += '<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>'
             }
 
             $('#daftar-menu').html(content)
