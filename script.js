eaa4e1df1208d34c4dadc318f172134966240b1d
sheanfinnegan <sheanfinnegan2905@gmail.com>
2021-10-05 08:57:22 +0700

    border card

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isiContentMenu() {
     $.getJSON("pizza.json", function (data) {
         let dataPizza = data.menu
         $.each(dataPizza, function (i, data) {
-            $('#daftar-menu').append('<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
+            $('#daftar-menu').append('<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5 shadow-lg border border-dark border-2" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>');
         })
     });
 }
@@ -36,7 +36,7 @@ $('.nav-link').on('click', function (e) {
 
         $.each(dataPizza, function (i, data) {
             if (data.kategori == isiNav.toLowerCase()) {
-                content += '<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>'
+                content += '<div class="col-lg-4 d-flex justify-content-center"><div class="card mb-5 shadow-lg border border-dark border-2" style="width: 18rem;"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><p>Rp. ' + data.harga + ',-</p><a href="#" class="btn btn-primary">Beli Sekarang</a></div></div></div>'
             }
 
             $('#daftar-menu').html(content)
